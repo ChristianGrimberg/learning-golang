ac45c7bc59a0f308b80f047b9f0078dd8da00c45
Christian Grimberg <christian.grimberg@outlook.com>
2021-02-10 14:25:25 -0300

    :+1: Aritmetic operations in Go.

--- importar.go
+++ importar.go
@@ -7,6 +7,8 @@ import (
 func main() {
 	const initial string = "Hello"
 	var name string = "world"
+	var num1 int = 10
+	var num2 int = 5
 
-	fmt.Printf("%s %s!", initial, name)
+	fmt.Printf("%s %s!\nResult: %d\n", initial, name, num1*num2)
 }

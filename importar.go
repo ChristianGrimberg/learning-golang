05d5ac784aa38acf8b26c7533ad7e5b171c0c22f
Christian Grimberg <christian.grimberg@outlook.com>
2021-02-10 14:50:29 -0300

    :+1: Using personal types.

--- importar.go
+++ importar.go
@@ -9,6 +9,22 @@ func main() {
 	var name string = "world"
 	var num1 int = 10
 	var num2 int = 5
+	var cup1 Coffee
+	var cup2 Coffee
 
-	fmt.Printf("%s %s!\nResult: %d\n", initial, name, num1*num2)
+	cup1 = Coffee{name: "Express", price: 5, sugar: true, milk: 250}
+	cup2 = Coffee{"Milk", 7, true, 350}
+
+	fmt.Printf("%s %s!\n", initial, name)
+	fmt.Printf("\nResult: %d\n", num1*num2)
+	fmt.Printf("\n==Coffee==\nName: %s\nPrice: %.2f\nSugar: %t\nMilk: %d\n", cup1.name, cup1.price, cup1.sugar, cup1.milk)
+	fmt.Printf("\n==Coffee==\nName: %s\nPrice: %.2f\nSugar: %t\nMilk: %d\n", cup2.name, cup2.price, cup2.sugar, cup2.milk)
+}
+
+// Coffee type
+type Coffee struct {
+	name  string
+	price float32
+	sugar bool
+	milk  int
 }

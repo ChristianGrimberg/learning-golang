a726ae75878c1e2ff0d305a8608a2e21aa26e127
Christian Grimberg <christian.grimberg@outlook.com>
2021-07-19 00:11:11 -0300

    :+1: [1] Workshop - Variables

--- main.go
+++ main.go
@@ -1,27 +1,29 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+func init() {
+	fmt.Printf("==Start with init==\n")
+}
 
 func main() {
-	const initial string = "Hello"
-	var name string = "world"
-	var num1 int = 10
-	var num2 int = 5
-	var cup1 Coffee
-	var cup2 Coffee
+	const initial string = "Hello" // Variables of constant type
+	var name string = "world"      // Default initialization
+	var num1, num2 int = 10, 5     // Multiple value assignment
+	var num3 = 20                  // Automatic type inference
+	price1, price2 := 40, 50.15    // Shorthand assignment
+	var cup1, cup2 Coffee
 
-	cup1 = Coffee{name: "Express", price: 5, sugar: true, milk: 250}
-	cup2 = Coffee{"Milk", 7, true, 350}
+	cup1 = Coffee{name: "Express", price: float32(price1), sugar: true, milk: 250}
+	cup2 = Coffee{"Milk", float32(price2), true, 350}
 
-	fmt.Printf("%s %s!\n", initial, name)
-	fmt.Printf("\nResult: %d\n", num1*num2)
-	fmt.Printf("\n==Coffee==\nName: %s\nPrice: %.2f\nSugar: %t\nMilk: %d\n", cup1.name, cup1.price, cup1.sugar, cup1.milk)
-	fmt.Printf("\n==Coffee==\nName: %s\nPrice: %.2f\nSugar: %t\nMilk: %d\n", cup2.name, cup2.price, cup2.sugar, cup2.milk)
-	fmt.Printf("\nMuliply %d x %d = %d\n", num1, num2, multiply(num1, num2))
-	value1, value2 := clousures(1)
-	fmt.Printf("\n%d %s\n", value1, value2)
+	fmt.Printf("=> %s %s!\n", initial, name)
+	fmt.Printf("=> Result: %d\n", num1*num2)
+	fmt.Printf("==Coffee==\nName: %s\nPrice: %.2f\nSugar: %t\nMilk: %d\n", cup1.name, cup1.price, cup1.sugar, cup1.milk)
+	fmt.Printf("==Coffee==\nName: %s\nPrice: %.2f\nSugar: %t\nMilk: %d\n", cup2.name, cup2.price, cup2.sugar, cup2.milk)
+	fmt.Printf("=> Muliply %d x %d = %d\n", num1, num2, multiply(num1, num2))
+	value1, value2 := clousures(num3)
+	fmt.Printf("=> %d %s\n", value1, value2)
 }
 
 // Coffee type
@@ -33,9 +35,7 @@ type Coffee struct {
 }
 
 func multiply(num1 int, num2 int) int {
-	var result int
-
-	result = num1 * num2
+	var result int = num1 * num2
 
 	return result
 }
@@ -45,5 +45,5 @@ func clousures(number int) (int, string) {
 		return number * 10
 	}
 
-	return variable(), " units"
+	return variable(), "units"
 }
